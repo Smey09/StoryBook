7d908fe085e4bb32af4ef5663c2a5df2237d9ea2
Smey <roemreaksmey7@gmail.com>
2024-08-16 09:40:27 +0700

    first commit

---
+++ app/components/TestingCard.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const TestingCard = () => {
+  return (
+    <div>
+      <div className="flex justify-center items-center h-[100px] w-auto bg-gray-300 text-black rounded-xl hover:bg-white">
+        <h1>Card testing</h1>
+        <div>
+          <img src="" alt="" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default TestingCard;
